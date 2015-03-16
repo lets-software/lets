8eb73855ab36223771e15b66a332974eb3628e8b
Gael Langlais <gael.langlais@papcart.fr>
2015-03-16 08:16:24 +0100

    Change indentation and code formating

--- includes/classes/comments.class.php
+++ includes/classes/comments.class.php
@@ -14,8 +14,8 @@ class comments {
         $error;
 
     function comments() {
-        $this->error = "No errors";
-        $this->added = false;
+        $this->error    = "No errors";
+        $this->added  = false;
         $this->set_required_variables();
     }
 
@@ -23,25 +23,24 @@ class comments {
         $this->id = '';
         $this->title = '';
         $this->comment = '';
-
     }
     function set_required_variables() {
         $mysql = new mysql;
         $mysql->result('SELECT require_comment_title , require_comment_body FROM config LIMIT 1');
-        $this->require_comment_title = $mysql->result['require_comment_title'];
+        $this->require_comment_title   = $mysql->result['require_comment_title'];
         $this->require_comment_body = $mysql->result['require_comment_body'];
     }
     function build_comment($id) {
         $mysql = new mysql;
         if ($mysql->result('SELECT * FROM comments WHERE commentID = '.$id.' LIMIT 1')) {
-            $this->id               =       $id;
-            $this->title            =       $mysql->result['title'];
+            $this->id                      =       $id;
+            $this->title                   =       $mysql->result['title'];
             $this->member_id        =       $mysql->result['accountID'];
-            $this->guest_name       =       $mysql->result['guest_name'];
+            $this->guest_name      =       $mysql->result['guest_name'];
             $this->comment          =       $mysql->result['comment'];
-            $this->article_id       =       $mysql->result['articleID'];
+            $this->article_id           =       $mysql->result['articleID'];
             $this->noticeboard_id   =       $mysql->result['noticeboardID'];
-            $this->event_id         =       $mysql->result['eventID'];
+            $this->event_id            =       $mysql->result['eventID'];
             return true;
         }
         return false;
@@ -50,9 +49,9 @@ class comments {
     function check_required($bool,$position) {
         if ($bool == 1) {
             if ($position == 1) {
-                return "<span class=\"required_field\">";
+                return '<span class="required_field">';
             } else {
-                return "</span>";
+                return '</span>';
             }
         }
     }
@@ -70,11 +69,11 @@ class comments {
         }
         if (!isset($post_post['comment_body'])) {
             if ($this->require_comment_body) {
-                $this->error .= ucfirst(COMMENT_NAME_SINGULAR).' body required<br />';
+                $this->error .= ucfirst(COMMENT_NAME_SINGULAR) . ' body required<br />';
             }
         } else {
             if (!$post_post['comment_body']) {
-                $this->error .= ucfirst(COMMENT_NAME_SINGULAR).' body required<br />';
+                $this->error .= ucfirst(COMMENT_NAME_SINGULAR) . ' body required<br />';
             }
             if (VALIDATE_XHTML) {
                 $xhtml_report = valid_XHTML($post_post['comment_body']);
@@ -142,50 +141,50 @@ class comments {
         if (strpos(' '.$action_page,URL)) {
             $action_page = str_replace(URL,'',$action_page);
         }
-        $z = $i.'<!-- comment_form -->'."\n";
-        $z .= $i.'<div id="comment_form">'."\n";
+        $z = '<!-- comment_form -->'."\n";
+        $z .= '<div id="comment_form">'."\n";
         if ($type == 'add') {
-            $z .= $i."<span class=\"add_comment_title\">Add ".a(COMMENT_NAME_SINGULAR)." ".ucwords(COMMENT_NAME_SINGULAR)."</span><br /><br />\n";
+            $z .= '<span class="add_comment_title">Add ' . a(COMMENT_NAME_SINGULAR) . ' ' . ucwords(COMMENT_NAME_SINGULAR) . '</span><br /><br />';
         }
-        $z .= $i."Required fields are <span class=\"required_field\">".REQUIRED_DISPLAY."</span>.<br /><br />\n";
-        $z .= $i."<fieldset><br />\n";
-        $z .= $i.' <form name="comment" method="post" action="'.URL.$action_page.append_url().'">'."\n";
+        $z .= 'Required fields are <span class="required_field">' . REQUIRED_DISPLAY . '</span>.<br /><br />';
+        $z .= '<fieldset><br />';
+        $z .= '<form name="comment" method="post" action="'.URL.$action_page.append_url().'">';
         if(!user_type() and ENABLE_GUEST_COMMENTS) {
-            $z .= $i.'  <label for="guest_name">Guest Name:</label>'."\n";
-            $z .= $i.'  <input type="text" id="guest_name" name="guest_name" maxlength="50" value="Guest" /><br class="left" />'."\n";
+            $z .= '  <label for="guest_name">Guest Name:</label>';
+            $z .= '  <input type="text" id="guest_name" name="guest_name" maxlength="50" value="Guest" /><br class="left" />';
         } elseif ($type == 'edit' and $this->guest_name) {
-            $z .= $i.'  <label for="guest_name">Guest Name:</label>'."\n";
-            $z .= $i.'  <input type="text" id="guest_name" name="guest_name" maxlength="50" value="'.$this->guest_name.'" /><br class="left" />'."\n";
+            $z .= '  <label for="guest_name">Guest Name:</label>';
+            $z .= '  <input type="text" id="guest_name" name="guest_name" maxlength="50" value="' . $this->guest_name . '" /><br class="left" />';
         }
-        $z .= $i.'  <label for="comment_title">'.$this->check_required($this->require_comment_title,1).'Title:'.$this->check_required($this->require_comment_title,2).'</label>'."\n";
-        $z .= $i.'  <input type="text" id="comment_title" name="comment_title" maxlength="255" value="'.htmlspecialchars($title).'" /><br class="left" />'."\n";
-        $z .= $i.'  <label for="comment_body">'.$this->check_required($this->require_comment_body,1).ucwords(COMMENT_NAME_SINGULAR).':'.$this->check_required($this->require_comment_body,2).'</label>'."\n";
-        $z .= $i.'  <textarea id="comment_body" name="comment_body">'.htmlspecialchars($this->comment)."</textarea>\n";
+        $z .= '  <label for="comment_title">' . $this->check_required($this->require_comment_title,1) . 'Title:' . $this->check_required($this->require_comment_title,2) . '</label>';
+        $z .= '  <input type="text" id="comment_title" name="comment_title" maxlength="255" value="' . htmlspecialchars($title) . '" /><br class="left" />';
+        $z .= '  <label for="comment_body">' . $this->check_required($this->require_comment_body,1) . ucwords(COMMENT_NAME_SINGULAR) . ':' . $this->check_required($this->require_comment_body,2) . '</label>';
+        $z .= '  <textarea id="comment_body" name="comment_body">' . htmlspecialchars($this->comment) . '</textarea>';
 
         if ($type == 'edit' and $this->id) {
-            $z .= $i.'  <input type="hidden" name="comment_id" value="'.$this->id.'" />'."\n";
+            $z .= '  <input type="hidden" name="comment_id" value="' . $this->id . '" />';
         }
         if ($noticeboard_id) {
-            $z .= $i.'  <input type="hidden" name="noticeboard_id" value="'.$noticeboard_id.'" />'."\n";
+            $z .= '  <input type="hidden" name="noticeboard_id" value="' . $noticeboard_id . '" />';
         }
         if ($article_id) {
-            $z .= $i.'  <input type="hidden" name="article_id" value="'.$article_id.'" />'."\n";
+            $z .= '  <input type="hidden" name="article_id" value="' . $article_id . '" />';
         }
         if ($event_id) {
-            $z .= $i.'  <input type="hidden" name="event_id" value="'.$event_id.'" />'."\n";
+            $z .= '  <input type="hidden" name="event_id" value="' . $event_id . '" />';
         }
         if ($type == 'add') {
-            $z .= $i.'  <input class="comment_button" type="submit" name="submit" value="Make '.ucwords(COMMENT_NAME_SINGULAR).'" />'."\n";
+            $z .= '  <input class="comment_button" type="submit" name="submit" value="Make ' . ucwords(COMMENT_NAME_SINGULAR) . '" />';
         } else {
-            $z .= $i.'  <input class="comment_button" type="submit" name="submit" value="Edit '.ucwords(COMMENT_NAME_SINGULAR).'" />'."\n";
+            $z .= '  <input class="comment_button" type="submit" name="submit" value="Edit ' . ucwords(COMMENT_NAME_SINGULAR) . '" />';
             if (ALLOW_COMMENT_DELETION) {
-                $z .= $i.'  <input class="comment_button" type="submit" name="submit" value="Delete '.ucwords(COMMENT_NAME_SINGULAR).'" />'."\n";
+                $z .= '  <input class="comment_button" type="submit" name="submit" value="Delete ' . ucwords(COMMENT_NAME_SINGULAR) . '" />';
             }
         }
-        $z .= $i.' </form>'."\n";
-        $z .= $i."</fieldset>\n";
-        $z .= $i.'</div>'."\n";
-        $z .= $i.'<!-- /comment_form -->'."\n";
+        $z .= ' </form>';
+        $z .= '</fieldset>';
+        $z .= '</div>';
+        $z .= '<!-- /comment_form -->';
         return $z;
 
     }
@@ -242,7 +241,7 @@ class comments {
     function edit_comment() {
         $mysql = new mysql;
         if ($this->id) {
-            $query = "UPDATE comments SET guest_name = '".mysql_real_escape_string($this->guest_name)."', title = '".mysql_real_escape_string($this->title)."', comment = '".mysql_real_escape_string($this->comment)."', edited_by = ".$_SESSION['member_id'].", edited_day = ".$GLOBALS['date']['day'].", edited_month = ".$GLOBALS['date']['month'].", edited_year = ".$GLOBALS['date']['year']." WHERE commentID = ".$this->id." LIMIT 1";
+            $query = "UPDATE comments SET guest_name = '" . mysql_real_escape_string($this->guest_name) . "', title = '" . mysql_real_escape_string($this->title) . "', comment = '" . mysql_real_escape_string($this->comment) . "', edited_by = " . $_SESSION['member_id'] . ", edited_day = " . $GLOBALS['date']['day'] . ", edited_month = " . $GLOBALS['date']['month'] . ", edited_year = " . $GLOBALS['date']['year'] . " WHERE commentID = " . $this->id . " LIMIT 1";
             if ($mysql->query($query)) {
                 return true;
             }
@@ -252,7 +251,7 @@ class comments {
     function delete_comment($id) {
         $mysql = new mysql;
         if (!$id) return false;
-        $query = "DELETE FROM comments WHERE commentID = ".$id." LIMIT 1";
+        $query = "DELETE FROM comments WHERE commentID = " . $id . " LIMIT 1";
         if ($mysql->query($query)) {
             return true;
         }
@@ -267,30 +266,30 @@ class comments {
         global $style;
         $z = '';
         $image_shown = false;
-        if ($mysql->build_array('SELECT * FROM comments WHERE noticeboardID = '.$noticeboard_id.' AND articleID = '.$article_id.' AND eventID = '.$event_id.' ORDER BY created_year, created_month, created_day, created_hour, created_minute')) {
+        if ($mysql->build_array('SELECT * FROM comments WHERE noticeboardID = ' . $noticeboard_id . ' AND articleID = ' . $article_id . ' AND eventID = ' . $event_id . ' ORDER BY created_year, created_month, created_day, created_hour, created_minute')) {
             if (!$mysql->num_rows) return '';
-            $z .= $i."<!-- comment_list -->\n";
-            $z .= $i."<div id=\"comment_list\">\n";
-            $z .= $i." <span class=\"comment_list_title\">".COMMENT_NAME."</span><br /><br />\n";
+            $z .= '<!-- comment_list -->';
+            $z .= '<div id="comment_list">';
+            $z .= '<span class="comment_list_title">' . COMMENT_NAME . '</span><br /><br />';
             foreach ($mysql->result as $comment) {
                 if (ENABLE_IMAGES and COMMENT_MEMBER_IMAGES and user_type() and !$_SESSION["member_suspended"] and $_SESSION["member_validated"]) {
                     if ($mysql->result('SELECT imageID FROM accounts WHERE accountID = '.$comment['accountID'].' LIMIT 1')) {
                         if ($mysql->result['imageID']) {
                             if ($image->img($mysql->result['imageID'],'t',$user->first_name($comment['accountID']),'comment_image')) {
-                                $z .= $i.' <a href="'.URL.MEMBER_LIST_URL.'/'.$comment['accountID'].'/'.append_url(0).'">'.$image->img."</a>\n";
+                                $z .= $i.' <a href="' . URL.MEMBER_LIST_URL . '/' . $comment['accountID'] . '/' . append_url(0) . '">' . $image->img . '</a>';
                                 $image_shown = true;
                             }
                         }
                     }
                 }
-                $z .= $i." <span class=\"comment_title\">".$comment['title']."</span><br />\n";
-                $z .= $i." <span class=\"comment_details\">Posted by ";
+                $z .= '<span class="comment_title">' . $comment['title'] . '</span><br />';
+                $z .= '<span class="comment_details">Posted by ';
                 if ((user_type() == 0 or $_SESSION["member_validated"] == 0 or $_SESSION["member_suspended"] == 1) and $comment['accountID']) {
-                    $z .= '<a href="'.URL.MEMBER_LIST_URL.'/'.$comment['accountID'].'/">'.ucwords(MEMBERS_NAME_SINGULAR).' '.$comment['accountID']."</a>";
+                    $z .= '<a href="' . URL.MEMBER_LIST_URL . '/' . $comment['accountID'] . '/">' . ucwords(MEMBERS_NAME_SINGULAR) . ' ' . $comment['accountID'] . "</a>";
                 } elseif ((user_type() == 1 or user_type() == 2) and $comment['accountID']) {
                     $name = $user->full_name($comment['accountID']);
-                    $z .= '<a href="'.URL.MEMBER_LIST_URL.'/'.$comment['accountID'].'/'.append_url(0).'">'.$name;
-                    $z .= "</a>";
+                    $z .= '<a href="' . URL.MEMBER_LIST_URL . '/' . $comment['accountID'] . '/' . append_url(0) . '">' . $name;
+                    $z .= '</a>';
                 } else {
                     if ($comment['guest_name']) {
                         $z .= $comment['guest_name'];
@@ -298,31 +297,31 @@ class comments {
                         $z .= 'Guest';
                     }
                 }
-                $z .= ' '.return_month($comment['created_month']).' '.$comment['created_day'].', '.$comment['created_year'].' - '.return_time($comment['created_hour'],$comment['created_minute']);
+                $z .= ' ' . return_month($comment['created_month']) . ' ' . $comment['created_day'] . ', ' . $comment['created_year'] . ' - ' . return_time($comment['created_hour'],$comment['created_minute']);
                 if (isset($_SESSION["member_id"])) {
                     if ((($comment['accountID'] == $_SESSION["member_id"]) or user_type() == 2) and $links->build_url(1,11)) {
-                        $z .= ' (<a href="'.URL.$links->complete_url.$comment['commentID'].'/'.append_url(0).'">edit</a>)';
+                        $z .= ' (<a href="' . URL . $links->complete_url . $comment['commentID'] . '/' . append_url(0) . '">edit</a>)';
                     }
                 }
-                $z .= "</span><br /><br />\n";
-                $z .= $i." <span class=\"comment_body\">\n".indent_variable($i.'  ',$comment['comment'])."\n";
+                $z .= '</span><br /><br />';
+                $z .= '<span class="comment_body">' . indent_variable(' ',$comment['comment']);
                 $z .= $i." </span><br />\n";
                 if (SHOW_COMMENT_EDITED and $comment['edited_by']) {
-                    $z .= $i." <span class=\"comment_details\">Last edited by ";
+                    $z .= '<span class="comment_details">Last edited by ';
                     if (user_type() == 0 or $_SESSION["member_validated"] == 0 or $_SESSION["member_suspended"] == 1) {
-                        $z .= '<a href="'.URL.MEMBER_LIST_URL.'/'.$comment['edited_by'].'/">'.ucwords(MEMBERS_NAME_SINGULAR).' '.$comment['edited_by']."</a>";
+                        $z .= '<a href="' . URL . MEMBER_LIST_URL . '/' . $comment['edited_by'] . '/">' . ucwords(MEMBERS_NAME_SINGULAR) . ' ' . $comment['edited_by'] . '</a>';
                     } elseif (user_type() == 1 or user_type() == 2) {
-                        $z .= '<a href="'.URL.MEMBER_LIST_URL.'/'.$comment['edited_by'].'/'.append_url(0).'">'.$user->first_name($comment['edited_by'])."</a>";
+                        $z .= '<a href="' . URL . MEMBER_LIST_URL . '/' . $comment['edited_by'] . '/' . append_url(0) . '">' . $user->first_name($comment['edited_by']) . '</a>';
                     }
-                    $z .= ' '.return_month($comment['edited_month']).' '.$comment['edited_day'].', '.$comment['edited_year']."</span><br />\n";
+                    $z .= ' ' . return_month($comment['edited_month']) . ' ' . $comment['edited_day'] . ', ' . $comment['edited_year'] . '</span><br />';
                 }
-                $z .= $i." <br />\n";
+                $z .= ' <br />';
 
             }
-            $z .= $i."</div>\n";
-            $z .= $i."<!-- /comment_list -->\n";
+            $z .= '</div>';
+            $z .= '<!-- /comment_list -->';
             if ($image_shown) {
-                $style->dynamic_elements .= " img.comment_image {float:left; width:".IMAGE_WIDTH_THUMB_MEMBER."; margin-right:5px; }\n";
+                $style->dynamic_elements .= ' img.comment_image {float:left; width:' . IMAGE_WIDTH_THUMB_MEMBER . '; margin-right:5px; }';
             }
             return $z;
         }
